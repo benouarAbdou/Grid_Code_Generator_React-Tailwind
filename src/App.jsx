21b4a99be64d6abc3248fb0d69b29759495dd4fb
benouarAbdou <benouarabdelouahab@gmail.com>
2025-06-26 13:17:41 +0200

    github link

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import GridGenerator from "./gridGenerator";
 import { Toaster } from "react-hot-toast";
+import GitHub from "./assets/git.png";
 
 function App() {
   return (
     <>
+      <img
+        src={GitHub}
+        alt="GitHub"
+        className="absolute top-0 right-0 w-20 h-20 z-50 cursor-pointer"
+        onClick={() =>
+          window.open(
+            "https://github.com/benouarAbdou/Grid_Code_Generator_React-Tailwind",
+            "_blank"
+          )
+        }
+      />
       <Toaster />
       <GridGenerator />
     </>
